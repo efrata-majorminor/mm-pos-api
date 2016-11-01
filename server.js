77894531d8f4f3375836599b661190d2bd8e225e
lioenel <kepin91@yahoo.co.id>
2016-11-01 13:46:59 +0700

    remove inventory

--- server.js
+++ server.js
@@ -5,34 +5,7 @@ var server = restify.createServer();
 server.use(restify.queryParser());
 server.use(restify.bodyParser()); 
 server.use(restify.CORS());
-   
-var originRouter = require('./src/routers/v1/inventory/storage-router');
-originRouter.applyRoutes(server, "v1/inventory/storages"); 
-    
-var storageInventoryRouter = require('./src/routers/v1/inventory/storage-inventory-router');
-storageInventoryRouter.applyRoutes(server, "v1/inventory/storages"); 
-   
-var storageInventoryMovementRouter = require('./src/routers/v1/inventory/storage-inventory-movement-router');
-storageInventoryMovementRouter.applyRoutes(server, "v1/inventory/storages"); 
 
-var transferInDocRouter = require('./src/routers/v1/inventory/transfer-in-doc-router');
-transferInDocRouter.applyRoutes(server, "v1/inventory/docs/transfer-in"); 
-
-var transferOutDocRouter = require('./src/routers/v1/inventory/transfer-out-doc-router');
-transferOutDocRouter.applyRoutes(server, "v1/inventory/docs/transfer-out"); 
-
-var inventoryReceiveModuleRouter = require('./src/routers/v1/inventory/inventory-receive-module-router');
-inventoryReceiveModuleRouter.applyRoutes(server, "v1/inventory/docs"); 
-
-var inventoryDocModuleRouter = require('./src/routers/v1/inventory/inventory-doc-module-router');
-inventoryDocModuleRouter.applyRoutes(server, "v1/inventory/docs"); 
-
-var merchandiserDocModuleSpecifyRouter = require('./src/routers/v1/merchandiser/merchandiser-doc-module-specify-router');
-merchandiserDocModuleSpecifyRouter.applyRoutes(server, "v1/merchandiser/docs");  
-
-var merchandiserDocModuleRouter = require('./src/routers/v1/merchandiser/merchandiser-doc-module-router');
-merchandiserDocModuleRouter.applyRoutes(server, "v1/merchandiser/docs");  
- 
 var masterStoreRouter = require('./src/routers/v1/master/store-router');
 masterStoreRouter.applyRoutes(server, "v1/master/stores");
 
