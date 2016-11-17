2355430f55214baaa5a79e55d153d19de4b13c8e
lioenel <kepin91@yahoo.co.id>
2016-11-17 19:55:23 +0700

    add API for sales return

---
+++ src/routers/v1/sales/sales-return-router.js
@@ -0,0 +1,154 @@
+var Router = require('restify-router').Router;;
+var router = new Router();
+var SalesReturnManager = require('bateeq-module').sales.SalesReturnManager;
+var db = require('../../../db');
+var resultFormatter = require("../../../result-formatter");
+var ObjectId = require('mongodb').ObjectId;
+
+const apiVersion = '1.0.0';
+
+router.get('/', (request, response, next) => {
+    db.get().then(db => {
+        var manager = new SalesReturnManager(db, {
+            username: 'router'
+        });
+
+        var query = request.query;
+
+        manager.read(query)
+            .then(docs => {
+                var result = resultFormatter.ok(apiVersion, 200, docs.data);
+                delete docs.data;
+                result.info = docs;
+                response.send(200, result);
+            })
+            .catch(e => {
+                var error = resultFormatter.fail(apiVersion, 400, e);
+                response.send(400, error);
+            })
+
+    })
+});
+
+router.get('/:id', (request, response, next) => {
+    db.get().then(db => {
+        var manager = new SalesReturnManager(db, {
+            username: 'router'
+        });
+
+        var id = request.params.id;
+
+        manager.getSingleById(id)
+            .then(doc => {
+                var result = resultFormatter.ok(apiVersion, 200, doc);
+                response.send(200, result);
+            })
+            .catch(e => {
+                var error = resultFormatter.fail(apiVersion, 400, e);
+                response.send(400, error);
+            })
+
+    })
+});
+
+router.get('/:storeid/:datefrom/:dateto', (request, response, next) => {
+    db.get().then(db => {
+        var manager = new SalesReturnManager(db, {
+            username: 'router'
+        });
+        // format date : yyyy/MM/dd
+        var storeid = request.params.storeid;
+        var datefrom = request.params.datefrom;
+        var dateto = request.params.dateto;
+
+        var query = request.query;
+        query.filter = {
+            storeId: new ObjectId(storeid),
+            date: {
+                $gte: new Date(datefrom),
+                $lte: new Date(dateto)
+            }
+        };
+
+        manager.read(query)
+            .then(docs => {
+                var result = resultFormatter.ok(apiVersion, 200, docs.data);
+                delete docs.data;
+                result.info = docs;
+                response.send(200, result);
+            })
+            .catch(e => {
+                var error = resultFormatter.fail(apiVersion, 400, e);
+                response.send(400, error);
+            })
+
+    })
+});
+
+router.post('/', (request, response, next) => {
+    db.get().then(db => {
+        var manager = new SalesReturnManager(db, {
+            username: 'router'
+        });
+
+        var data = request.body;
+
+        manager.create(data)
+            .then(docId => {
+                response.header('Location', `salesreturn/docs/sales/${docId.toString()}`);
+                var result = resultFormatter.ok(apiVersion, 201);
+                response.send(201, result);
+            })
+            .catch(e => {
+                var error = resultFormatter.fail(apiVersion, 400, e);
+                response.send(400, error);
+            })
+
+    })
+});
+
+router.put('/:id', (request, response, next) => {
+    db.get().then(db => {
+        var manager = new SalesReturnManager(db, {
+            username: 'router'
+        });
+
+        var id = request.params.id;
+        var data = request.body;
+
+        manager.update(data)
+            .then(docId => {
+                var result = resultFormatter.ok(apiVersion, 204);
+                response.send(204, result);
+            })
+            .catch(e => {
+                var error = resultFormatter.fail(apiVersion, 400, e);
+                response.send(400, error);
+            })
+
+    })
+});
+
+router.del('/:id', (request, response, next) => {
+    db.get().then(db => {
+        var manager = new SalesReturnManager(db, {
+            username: 'router'
+        });
+
+        var id = request.params.id;
+        var data = request.body;
+
+        manager.delete(data)
+            .then(docId => {
+                var result = resultFormatter.ok(apiVersion, 204);
+                response.send(204, result);
+            })
+            .catch(e => {
+                var error = resultFormatter.fail(apiVersion, 400, e);
+                response.send(400, error);
+            })
+    })
+});
+
+
+module.exports = router;
