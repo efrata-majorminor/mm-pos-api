e2d3c900d84e79c2b0c285876d8f4d33a2afb734
riandp <riandwiputra@yahoo.com>
2016-11-24 09:33:43 +0700

    update api

--- src/routers/v1/sales/sales-module-router.js
+++ src/routers/v1/sales/sales-module-router.js
@@ -1,6 +1,6 @@
 var Router = require('restify-router').Router;;
 var router = new Router();
-var PaymentManager = require('bateeq-module').sales.SalesManager;
+var SalesManager = require('bateeq-module').sales.SalesManager;
 var db = require('../../../db');
 var resultFormatter = require("../../../result-formatter");
 var ObjectId = require('mongodb').ObjectId;
@@ -9,7 +9,7 @@ const apiVersion = '1.0.0';
 
 router.get('/', (request, response, next) => {
     db.get().then(db => {
-        var manager = new PaymentManager(db, {
+        var manager = new SalesManager(db, {
             username: 'router'
         });
 
@@ -32,7 +32,7 @@ router.get('/', (request, response, next) => {
 
 router.get('/:id', (request, response, next) => {
     db.get().then(db => {
-        var manager = new PaymentManager(db, {
+        var manager = new SalesManager(db, {
             username: 'router'
         });
 
@@ -51,19 +51,148 @@ router.get('/:id', (request, response, next) => {
     })
 });
 
-router.get('/:storeid/:datefrom/:dateto', (request, response, next) => {
+router.get('/:storename/:isTransByStoreName', (request, response, next) => {
     db.get().then(db => {
-        var manager = new PaymentManager(db, {
+        var manager = new SalesManager(db, {
+            username: 'router'
+        });
+
+        var storename = request.params.storename;
+
+        var query = request.query;
+        query.filter = {
+            "store.name": storename.toString()
+            
+        };
+        manager.read(query)
+            .then(docs => {
+                var result = resultFormatter.ok(apiVersion, 200, docs.data);
+                delete docs.data;
+                result.info = docs;
+                response.send(200, result);
+            })
+            .catch(e => {
+                var error = resultFormatter.fail(apiVersion, 400, e);
+                response.send(400, error);
+            })
+
+    })
+});
+
+//getAllSalesByVoidTrue
+router.get('/:allDate/:allCode/:allStore', (request, response, next) => {
+    db.get().then(db => {
+        var manager = new SalesManager(db, {
+            username: 'router'
+        });
+        // format date : yyyy/MM/dd
+
+        var query = request.query;
+        query.filter = {
+            isVoid : true
+        };
+
+        manager.read(query)
+            .then(docs => {
+                var result = resultFormatter.ok(apiVersion, 200, docs.data);
+                delete docs.data;
+                result.info = docs;
+                response.send(200, result);
+            })
+            .catch(e => {
+                var error = resultFormatter.fail(apiVersion, 400, e);
+                response.send(400, error);
+            })
+
+    })
+});
+
+//getAllSalesByFilter
+router.get('/:storeid/:datefrom/:dateto/:shift', (request, response, next) => {
+    db.get().then(db => {
+        var manager = new SalesManager(db, {
             username: 'router'
         });
         // format date : yyyy/MM/dd
         var storeid = request.params.storeid;
         var datefrom = request.params.datefrom;
         var dateto = request.params.dateto;
+        var shift = request.params.shift;
 
         var query = request.query;
         query.filter = {
             storeId: new ObjectId(storeid),
+            date: {
+                $gte: new Date(datefrom),
+                $lte: new Date(dateto)
+            },
+            shift : shift.toString()
+        };
+
+        manager.read(query)
+            .then(docs => {
+                var result = resultFormatter.ok(apiVersion, 200, docs.data);
+                delete docs.data;
+                result.info = docs;
+                response.send(200, result);
+            })
+            .catch(e => {
+                var error = resultFormatter.fail(apiVersion, 400, e);
+                response.send(400, error);
+            })
+
+    })
+});
+
+//getAllSalesAllStore = 5 parameter
+router.get('/:storeid/:datefrom/:dateto/:shift/:typeAllStore', (request, response, next) => {
+    db.get().then(db => {
+        var manager = new SalesManager(db, {
+            username: 'router'
+        });
+        // format date : yyyy/MM/dd
+        var datefrom = request.params.datefrom;
+        var dateto = request.params.dateto;
+        var shift = request.params.shift;
+
+        var query = request.query;
+        query.filter = {
+            date: {
+                $gte: new Date(datefrom),
+                $lte: new Date(dateto)
+            },
+            shift : shift.toString()
+        };
+
+        manager.read(query)
+            .then(docs => {
+                var result = resultFormatter.ok(apiVersion, 200, docs.data);
+                delete docs.data;
+                result.info = docs;
+                response.send(200, result);
+            })
+            .catch(e => {
+                var error = resultFormatter.fail(apiVersion, 400, e);
+                response.send(400, error);
+            })
+
+    })
+});
+
+//getAllSalesAllShift = 6 parameter
+router.get('/:storename/:datefrom/:dateto/:shift/:typeAllStore/:typeAllShift', (request, response, next) => {
+    db.get().then(db => {
+        var manager = new SalesManager(db, {
+            username: 'router'
+        });
+        // format date : yyyy/MM/dd
+        var storename = request.params.storename;
+        var datefrom = request.params.datefrom;
+        var dateto = request.params.dateto;
+
+        var query = request.query;
+        query.filter = {
+            'store.name': storename.toString(),
             date: {
                 $gte: new Date(datefrom),
                 $lte: new Date(dateto)
@@ -85,9 +214,107 @@ router.get('/:storeid/:datefrom/:dateto', (request, response, next) => {
     })
 });
 
+//getAllSalesAllStoreAllShift = 7 parameter
+router.get('/:storename/:datefrom/:dateto/:shift/:typeAllStore/:typeAllShift/:typeAllStoreAllShift', (request, response, next) => {
+    db.get().then(db => {
+        var manager = new SalesManager(db, {
+            username: 'router'
+        });
+        // format date : yyyy/MM/dd
+        var datefrom = request.params.datefrom;
+        var dateto = request.params.dateto;
+
+        var query = request.query;
+        query.filter = {
+            date: {
+                $gte: new Date(datefrom),
+                $lte: new Date(dateto)
+            }
+        };
+
+        manager.read(query)
+            .then(docs => {
+                var result = resultFormatter.ok(apiVersion, 200, docs.data);
+                delete docs.data;
+                result.info = docs;
+                response.send(200, result);
+            })
+            .catch(e => {
+                var error = resultFormatter.fail(apiVersion, 400, e);
+                response.send(400, error);
+            })
+
+    })
+});
+
+//getAllSalesWithSpecificFilter = 8 parameter
+router.get('/:storename/:datefrom/:dateto/:shift/:typeAllStore/:typeAllShift/:typeAllStoreAllShift/:typeNoStoreNoShift', (request, response, next) => {
+    db.get().then(db => {
+        var manager = new SalesManager(db, {
+            username: 'router'
+        });
+        // format date : yyyy/MM/dd
+        var storename = request.params.storename;
+        var datefrom = request.params.datefrom;
+        var dateto = request.params.dateto;
+        var shift = request.params.shift;
+
+        var query = request.query;
+        query.filter = {
+            'store.name': storename.toString(),
+            date: {
+                $gte: new Date(datefrom),
+                $lte: new Date(dateto)
+            },
+            shift : shift.toString()
+        };
+
+        manager.read(query)
+            .then(docs => {
+                var result = resultFormatter.ok(apiVersion, 200, docs.data);
+                delete docs.data;
+                result.info = docs;
+                response.send(200, result);
+            })
+            .catch(e => {
+                var error = resultFormatter.fail(apiVersion, 400, e);
+                response.send(400, error);
+            })
+
+    })
+});
+
+//getAllSalesByReference = 9 parameter
+router.get('/:storename/:datefrom/:dateto/:shift/:typeAllStore/:typeAllShift/:typeAllStoreAllShift/:typeNoStoreNoShift/:refer', (request, response, next) => {
+    db.get().then(db => {
+        var manager = new SalesManager(db, {
+            username: 'router'
+        });
+        // format date : yyyy/MM/dd
+        var refer = request.params.refer;
+
+        var query = request.query;
+        query.filter = {
+            code : refer
+        };
+
+        manager.read(query)
+            .then(docs => {
+                var result = resultFormatter.ok(apiVersion, 200, docs.data);
+                delete docs.data;
+                result.info = docs;
+                response.send(200, result);
+            })
+            .catch(e => {
+                var error = resultFormatter.fail(apiVersion, 400, e);
+                response.send(400, error);
+            })
+
+    })
+});
 router.post('/', (request, response, next) => {
     db.get().then(db => {
-        var manager = new PaymentManager(db, {
+        var manager = new SalesManager(db, {
             username: 'router'
         });
 
@@ -105,18 +332,18 @@ router.post('/', (request, response, next) => {
             })
 
     })
-});
+}); 
 
-router.put('/:id', (request, response, next) => {
+router.put('/', (request, response, next) => {
     db.get().then(db => {
-        var manager = new PaymentManager(db, {
+        var manager = new SalesManager(db, {
             username: 'router'
         });
 
         var id = request.params.id;
         var data = request.body;
 
-        manager.update(data)
+        manager._void(data)
             .then(docId => {
                 var result = resultFormatter.ok(apiVersion, 204);
                 response.send(204, result);
@@ -127,11 +354,12 @@ router.put('/:id', (request, response, next) => {
             })
 
     })
-});
+}); 
+
 
 router.del('/:id', (request, response, next) => {
     db.get().then(db => {
-        var manager = new PaymentManager(db, {
+        var manager = new SalesManager(db, {
             username: 'router'
         });
 
