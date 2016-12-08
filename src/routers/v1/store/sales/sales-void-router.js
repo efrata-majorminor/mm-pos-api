06bde6f42b640dcbada7aadffdc47408109fabf3
lioenel <kepin91@yahoo.co.id>
2016-12-08 14:49:50 +0700

    add api to get void is false

--- src/routers/v1/store/sales/sales-void-router.js
+++ src/routers/v1/store/sales/sales-void-router.js
@@ -51,5 +51,43 @@ router.get('/', passport, (request, response, next) => {
 
     })
 });
+
+
+router.get('/voidables', passport, (request, response, next) => {
+    db.get().then(db => {
+        var manager = new SalesManager(db, request.user);
+
+        var storeid = request.params.storeid;
+        
+        var query = request.query;
+        query.filter = !query.filter ? {} : JSON.parse(query.filter);
+        var filterStore = {
+            'storeId' : new ObjectId(storeid)
+        } 
+        var filter = {
+            isVoid: false
+        }; 
+        query.filter = {
+            '$and': [
+                query.filter,
+                filterStore,
+                filter
+            ]
+        };  
+
+        manager.read(query)
+            .then(docs => {
+                var result = resultFormatter.ok(apiVersion, 200, docs.data);
+                delete docs.data;
+                result.info = docs;
+                response.send(200, result);
+            })
+            .catch(e => {
+                var error = resultFormatter.fail(apiVersion, 400, e);
+                response.send(400, error);
+            })
+
+    })
+});
   
 module.exports = router;
