40bea1a1f440632e1c9a336a235cc219a3af79dc
lioenel <kepin91@yahoo.co.id>
2016-12-21 10:34:27 +0700

    add api router

--- src/routers/v1/store/sales/sales-return-router.js
+++ src/routers/v1/store/sales/sales-return-router.js
@@ -1,5 +1,6 @@
 var Router = require('restify-router').Router;;
 var router = new Router();
+var SalesManager = require('bateeq-module').sales.SalesManager;
 var SalesReturnManager = require('bateeq-module').sales.SalesReturnManager;
 var db = require('../../../../db');
 var resultFormatter = require("../../../../result-formatter");
@@ -49,4 +50,45 @@ router.get('/', passport, (request, response, next) => {
     })
 });
   
+router.get('/returnables', passport, (request, response, next) => {
+    db.get().then(db => {
+        var manager = new SalesManager(db, request.user);
+
+        var storeid = request.params.storeid;
+        
+        var query = request.query;
+        query.filter = !query.filter ? {} : JSON.parse(query.filter);
+        var filterStore = {
+            'storeId' : new ObjectId(storeid)
+        } 
+        var filter = {
+            'isVoid' : false
+        } 
+        query.filter = {
+            '$and': [
+                query.filter,
+                filterStore,
+                filter
+            ]
+        }; 
+        
+        query.order = {
+            '_updatedDate' : -1
+        }
+        
+        manager.read(query)
+            .then(docs => {
+                var result = resultFormatter.ok(apiVersion, 200, docs.data);
+                delete docs.data;
+                result.info = docs;
+                response.send(200, result);
+            })
+            .catch(e => {
+                var error = resultFormatter.fail(apiVersion, 400, e);
+                response.send(400, error);
+            })
+
+    })
+});
+
 module.exports = router;
