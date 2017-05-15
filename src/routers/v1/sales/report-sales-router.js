6de908f827b9ba95de1d34461a5954aa3af6cdde
danielmoonlay <daniel.nababan@moonlay.com>
2017-05-15 12:11:48 +0700

    daily report product

--- src/routers/v1/sales/report-sales-router.js
+++ src/routers/v1/sales/report-sales-router.js
@@ -76,4 +76,47 @@ router.get('/stores-product/:datefrom/:dateto/:productId', passport, (request, r
     })
 });
 
+router.get('/products/:datefrom/:dateto', passport, (request, response, next) => {
+    db.get().then(db => {
+        var manager = new SalesReportManager(db, request.user);
+        // format date : yyyy/MM/dd
+        var datefrom = request.params.datefrom;
+        var dateto = request.params.dateto;
+        var page = request.query.page || 1;
+        var size = request.query.size || 15;
+        var groupby = request.params.groupby || "";
+
+        var result = {};
+        var query = {};
+
+        var skip = (page - 1) * size;
+
+        result = manager.productsReport(datefrom, dateto, skip, size).toArray();
+        query = manager.productsReportQuery(datefrom, dateto).toArray();
+
+        Promise.all([result, query]).then((docs) => {
+            var query = {};
+            if (docs[1][0]) {
+                var count = docs[1][0].total;
+                query = {
+                    size: size,
+                    page: page,
+                    total: count,
+                    totalPage: Math.ceil(count / size)
+                }
+            }
+            var returnValue = {
+                query: query,
+                data: docs[0]
+            }
+            var result = resultFormatter.ok(apiVersion, 200, returnValue);
+            result.info = returnValue;
+            response.send(200, result);
+        }).catch(e => {
+            var error = resultFormatter.fail(apiVersion, 400, e);
+            response.send(400, error);
+        })
+    })
+});
+
 module.exports = router;
